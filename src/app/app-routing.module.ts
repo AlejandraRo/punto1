819de0c5b17a5ad71452049a878ed25586c0aadd
alejandra <lejanamar@gmail.com>
2022-09-02 20:01:34 -0500

    Punto 1 correspondiente al aplicativo heroes con el metodo de busqueda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HomeComponent } from './components/home/home.component';
+import { Heroe } from './servicios/heroes.service';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'heroes',component:HeroesComponent},
+  {path:'heroe/:id',component:HeroeComponent},
+  {path:'buscar/:termino',component:BuscadorComponent},    
+  {path:'**',pathMatch:'full',redirectTo:'home'}
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
